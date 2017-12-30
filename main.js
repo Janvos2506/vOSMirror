8b29a166e99cbce7e4cbac7d8cfcb1725b4fea6e
Jan <JB.Vos@student.han.nl>
2017-12-30 23:01:54 +0100

    fullscreen

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ function createWindow () {
   win.on('closed', function () {
     win = null
   })
-  win.setFullScreen(true);
+  //win.setFullScreen(true);
 }
 // Create window on electron intialization
 app.on('ready', createWindow)
